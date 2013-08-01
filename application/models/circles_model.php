ff6f57d37799d084d465715210cabcc5b42f6540
Sean Oh <sean@click3x.com>
2013-08-01 11:26:09 -0400

    Create rss feed controller

---
+++ application/models/circles_model.php
@@ -0,0 +1,20 @@
+<?php
+
+class Circles_Model extends BCA_Model
+{
+	public function Circles_Model()
+	{
+		$this->table = "circles";
+		$this->pk = "id";
+    	$this->fields = array(
+			'id' 							=> array("shown"=>true, 	"label"=>"Id"),
+			'users_fb_id' 					=> array("shown"=>true, 	"label"=>"User Facebook ID"),
+			'users_name'					=> array("shown"=>true, 	"label"=>"User's name"),
+			'users_photo_url'				=> array("shown"=>true, 	"label"=>"User's Profile Photo"),
+			'goal'							=> array("shown"=>true, 	"label"=>"Goal"),
+			'date'							=> array("shown"=>true, 	"label"=>"Date & time")
+		);
+	}
+}
+
+?>
