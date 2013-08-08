6cbf44b8a936f14343209fd37f550e4b0c3b44d2
Mili Kuo <mili@click3x.com>
2013-08-08 12:36:06 -0400

    facebook init

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -35,32 +35,6 @@
 		  var fbAppId = '<?php echo config_item("fb_app_id"); ?>';
 		  var baseUrl = '<?php echo base_url(); ?>';
 
-		  // This is boilerplate code that is used to initialize the Facebook
-		  // JS SDK.  You would normally set your App ID in this code.
-		
-		  // Additional JS functions here
-		  window.fbAsyncInit = function() {
-		    FB.init({
-		      appId      : fbAppId,        // App ID
-		      status     : true,           // check login status
-		      cookie     : true,           // enable cookies to allow the server to access the session
-		      xfbml      : true            // parse page for xfbml or html5 social plugins like login button below
-		    });
-		    
-		    checkLoginStatus();
-		
-		    // Put additional init code here
-		  };
-		
-		  // Load the SDK Asynchronously
-		  (function(d, s, id){
-		     var js, fjs = d.getElementsByTagName(s)[0];
-		     if (d.getElementById(id)) {return;}
-		     js = d.createElement(s); js.id = id;
-		     js.src = "//connect.facebook.net/en_US/all.js";
-		     fjs.parentNode.insertBefore(js, fjs);
-		   }(document, 'script', 'facebook-jssdk'));
-
 		</script>
     	
         <!--[if lt IE 7]>
--- js/main.js
+++ js/main.js
@@ -40,6 +40,8 @@ var PHOTO_COLUMN_NUM		= 4;
 
 $(document).ready(function(){	
 
+	initFacebook();
+
 	enableButtons();
 	enableEventBinds();
 	gallery.loadGallery();
@@ -55,6 +57,34 @@ $(document).ready(function(){
 	});
 
 });
+
+function initFacebook(){
+// This is boilerplate code that is used to initialize the Facebook
+	  // JS SDK.  You would normally set your App ID in this code.
+	
+	  // Additional JS functions here
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : fbAppId,        // App ID
+	      status     : true,           // check login status
+	      cookie     : true,           // enable cookies to allow the server to access the session
+	      xfbml      : true            // parse page for xfbml or html5 social plugins like login button below
+	    });
+	    
+	    checkLoginStatus();
+	
+	    // Put additional init code here
+	  };
+	
+	  // Load the SDK Asynchronously
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/all.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+}
 
 
 function checkLoginStatus(){
