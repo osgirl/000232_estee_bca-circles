db70c7877c862586ec03c372fc707f46fe543f64
Mili Kuo <mili@click3x.com>
2013-08-18 01:48:21 -0400

    filter done except friends circle

--- js/util/gallery.js
+++ js/util/gallery.js
@@ -51,6 +51,8 @@ function Gallery()
 
 		var allPhotoData;
 
+		var photoButtonHtml = '<div class="photo_rollover item_rollover"><div class="rollover_content"><div class="pink_btn all_cap view_circle_btn">view</div></div></div><div class="gallery_item_btn"></div>'
+
 
 		//--------------------------------------
 		//+ PRIVATE & PROTECTED INSTANCE METHODS
@@ -135,55 +137,6 @@ function Gallery()
 			}
 		}
 
-		function loadInitialCircles(isCircle) {
-			
-			// feed_circles.get({
-		 //    	limit:4,
-		 //    	success: function(self, data) {
-		 //    		if (data.response.updates.length > 0) {
-			//             parseCircleData(data.response.updates);
-			//             if(!isCircle)loadInitialPhotos();
-			//         }
-			//         else {
-			//             window.alert('No updates found!')
-			//         }
-		 //    	}
-		 //    });	
-		    			
-		}
-
-		function loadInitialPhotos() {
-
-			feed_photos.get({
-		    	limit:8,
-		    	success: function(self, data) {
-		    		if (data.response.updates.length > 0) {
-
-		    			//switch(currentFilterType){
-		    				// case "photo":
-		    				// 	parsePhotoData(photoFeed);
-		    				// break;
-		    				// case "instagram":
-		    				// 	parsePhotoData(instagramFeed);
-		    				// break;
-		    				// case "twitter":
-		    				// 	parsePhotoData(twitterFeed);
-		    				// break;
-		    				// default:
-		    					parsePhotoData(data.response.updates);
-		    				//break;
-		    			//}
-			            
-			        }
-			        else {
-			            window.alert('No updates found!')
-			        }
-		    	}
-		    });	
-		    			
-		}
-
-
 		function parseCircleData(data){
 			var feed;
 			var containerCount = 0;
@@ -225,20 +178,22 @@ function Gallery()
 				            			circleDiv.append(layoutData)
 				            			         .addClass('span6 circle_container gallery_item flex_margin_bottom');
 				            		$('.layout_circle').append(circleDiv);
+				            		$(circleDiv).hide();
+				            		$(circleDiv).fadeIn(200);
+
+				            		var contentData = {
+										index:i,
+										item:$(circleDiv),
+										totalNum:data.length,
+										colNum:CIRCLE_LAYOUT_COLUMN_NUM
+									}
 
 									populateCircleContent($(circleDiv), circleFeedDataArray[containerCount]);
 
-									if(containerCount == data.length-1){
-
-										if(i%CIRCLE_LAYOUT_COLUMN_NUM == 1) $(circleDiv).css('margin-right', '0');
-										
-										var rowNum = Math.ceil(containerCount/CIRCLE_LAYOUT_COLUMN_NUM);
-										var getHeight = ($(circleDiv).height() + 70)*i;
-										$('#gallery').height(getHeight);
-									}
+									if(containerCount == data.length-1) 
+										updateGalleryLayout(contentData);
 
 									containerCount++;
-		
 				             	}
 				      		});
 
@@ -254,7 +209,6 @@ function Gallery()
 
 		function populateCircleContent(circle, data){
 
-			circle.fadeIn(200);
 			circle.attr('circle_id', data.circle_id);
 			circle.find('.circle_creator').html(data.user_name);
 			circle.find('.circle_goal').html("<b>We Will - </b><br />" + data.goal);
@@ -273,10 +227,9 @@ function Gallery()
 					num_friends: data.friends_data.length
 				}}
 
-        	enableItemButtons(circle, popupData);
+        	enableItemButton(circle, popupData);
 
         	centerRollOverContent();
-			
 
 		}
 
@@ -287,7 +240,216 @@ function Gallery()
 			$(window).unbind('scroll');
 		}
 
+		// function parsePhotoData(data){
+
+		// }
+
+		function photoDiv(index){
+
+			var div;
+
+			if(currentFilterType == "all") {
+				div = $($('.photo_container').get(index));
+			}else{
+				div = $('<div>');
+				div.addClass('span3 photo_container gallery_item flex_margin_bottom');
+				div.appendTo('.layout_photo');
+
+				div.hide();
+				div.fadeIn(200);
+			}
+
+			return div;
+			
+		}
+
+		function getAllFeed(){
+
+			allPhotoData = new Array();
+
+			$.feed.get('bca-circle', parseCircleData, 3);
+			$.feed.get('bca-photo', handleAllPhotoData, 3);
+			$.feed.get('bca-twitter', handleAllPhotoData, 3);
+			$.feed.get('bca-instagram', handleAllPhotoData, 3);
+
+		}
+
 		function parsePhotoData(data){
+			var feed;
+
+			$(data).each(function(i){
+				feed = data[i].data;
+
+				$.ajax({
+		        		type: 'post',
+		            	url: baseUrl + 'photo/fetchUploadedPhotoData',
+		            	dataType: 'json',
+		            	data: {
+		            		photo_id: feed.text
+		            	},
+		            	success: function(dbData) {  
+
+		            		var photoIcon = baseUrl + "img/icons/bca.png";          
+
+		                	var popupData = {
+									type:'photo', 
+									data:{
+										source:'local', 
+										author:'John Doe',
+										content:dbData.description,
+										photo_url:baseUrl + "uploads/" + dbData.filename
+									}}
+		                	var html = "<img class='full_photo' src='" + baseUrl + "uploads/" + dbData.filename + "'/><img class='photo_icon' src='" + photoIcon + "'/>" + photoButtonHtml;
+
+							var contentData = {
+								index:i,
+								item:photoDiv(i),
+								totalNum:data.length,
+								type:'photo',
+								content:html,
+								popupData:popupData,
+								colNum:PHOTO_LAYOUT_COLUMN_NUM
+							}
+
+							populatePhotoContent(contentData);
+
+		             	}
+		      		});
+				});
+
+		}
+
+		function parseInstagramData(data){
+
+			var feed;
+
+			$(data).each(function(i){
+				feed = data[i].data;
+
+				var popupData = {
+						type:'photo', 
+						data:{
+							source:'instagram', 
+							author: feed.author.alias,
+							content:feed.text,
+							photo_url:feed.photos[0].url
+						}}
+
+				var photoIcon = baseUrl + "img/icons/instagram.png";
+
+				var html = "<img class='full_photo' src='" + feed.photos[0].url + "'/><img class='photo_icon' src='" + photoIcon + "'/>" + photoButtonHtml;
+
+				var contentData = {
+					index:i,
+					item:photoDiv(i),
+					totalNum:data.length,
+					type:'instagram',
+					content:html,
+					popupData:popupData,
+					colNum:PHOTO_LAYOUT_COLUMN_NUM
+				}
+
+				populatePhotoContent(contentData);
+
+			})
+
+		}
+
+		function parseTwitterData(data){
+
+			var feed;
+
+			$(data).each(function(i){
+				feed = data[i].data;
+
+				var popupData = {
+							type:'twitter', 
+							data:{
+								author:feed.author.alias, 
+								content:feed.text,
+								datetime:feed.timestamp,
+								avatar:feed.author.avatar
+							}}
+
+				var photoIcon = baseUrl + "img/icons/twitter-large.png";
+
+				var html = "<div class='twitter_avatar'><img src='" + feed.author.avatar + "'/></div>"
+					html	+= "<div class='twitter_title'><div class='twitter_author'>"+ feed.author.alias + "</div>"
+					html	+= "<div class='twitter_time'>"+ feed.timestamp + "</div></div>"
+					html	+= "<div class='twitter_text'>"+ feed.text + "</div>"
+					html 	+= "<img class='photo_icon' src='" + photoIcon + "'/>" + photoButtonHtml;
+
+				var contentData = {
+					index:i,
+					item:photoDiv(i),
+					totalNum:data.length,
+					type:'twitter',
+					content:html,
+					popupData:popupData,
+					colNum:PHOTO_LAYOUT_COLUMN_NUM
+				}
+
+				populatePhotoContent(contentData);
+
+
+				// var fcontent = "<div class='featured_twitter_avatar'><img src='" + feed.author.avatar + "'/></div>"
+				// 	fcontent	+= "<div class='featured_twitter_title'><div class='featured_twitter_author'>"+ feed.author.alias + "</div>"
+				// 	fcontent	+= "<div class='featured_twitter_time'>"+ feed.timestamp + "</div></div>"
+				// 	fcontent	+= "<div class='featured_twitter_text'>"+ feed.text + "</div>"
+				// 	fcontent += "<img class='photo_icon' src='" + photoIcon + "'/>" + photoButtonHtml;
+
+			})
+
+		}
+
+
+		function populatePhotoContent(contentData){
+
+			if(contentData.type == "twitter")
+				contentData.item.css('background', '#2caae1');
+
+			contentData.item.html(contentData.content);
+			contentData.item.attr('type', contentData.type);
+			contentData.item.hide();
+			contentData.item.fadeIn(200);
+
+			updateGalleryLayout(contentData);
+			
+			enableItemButton(contentData.item, contentData.popupData);
+			setTimeout(centerRollOverContent, 200);
+			//centerRollOverContent();
+		}
+
+		function updateGalleryLayout(contentData){
+
+			if(contentData.index%contentData.colNum == contentData.colNum-1) 
+				$(contentData.item).css('margin-right', '0');
+										
+			var rowNum = Math.ceil(contentData.totalNum/contentData.colNum);
+			var getHeight = ($(contentData.item).height() + 70)*rowNum;
+
+			if(getHeight > 600) {
+				$('#gallery').height(getHeight);
+			}else{
+				$('#gallery').height(600);
+			}
+
+		}
+
+		function enableItemButton(item, popupData){
+			$(item.find('.gallery_item_btn')).unbind('mouseover').mouseover(function(e){
+				$(e.currentTarget).css('cursor','pointer');
+				$(e.currentTarget).prev('.item_rollover').fadeIn(200);
+			})
+
+			$(item.find('.gallery_item_btn')).unbind('mouseout').mouseout(function(e){
+				$(e.currentTarget).prev('.item_rollover').fadeOut(200);
+			})
+
+        	$(item.find('.gallery_item_btn')).click(function(e){openPopUp(popupData)})
+		}
+
+		function parseAllPhotoData(data){
 
 			var feed;
 
@@ -295,7 +457,6 @@ function Gallery()
 				feed = data[i].data;
 
 				var div = $($('.photo_container').get(i));
-				var feature_div = $($('.feature_photo').get(i));
 				var popupData;
 				var photoIcon;
 				var html;
@@ -327,12 +488,11 @@ function Gallery()
 			                	html = "<img class='full_photo' src='" + baseUrl + "uploads/" + data.filename + "'/><img class='photo_icon' src='" + photoIcon + "'/>" + photoButtonHtml;
 			                	div.html(html);
 			                	div.attr('type', 'photo');
-			                	feature_div.html(html);
 
 			                	div.fadeIn(200);
 			                	gallery_container.show();
 								
-								enableItemButtons(div, popupData);
+								enableItemButton(div, popupData);
 
 								centerRollOverContent();
 
@@ -345,7 +505,6 @@ function Gallery()
 						//div.text('author: ' + feed.author.alias); // <-- author
 						//console.log(feed.text); 				  // <-- content
 						//console.log("instagram", feed.photos[0].url); 	// <-- photo_url
-
 
 						popupData = {
 									type:'photo', 
@@ -361,11 +520,10 @@ function Gallery()
 						html = "<img class='full_photo' src='" + feed.photos[0].url + "'/><img class='photo_icon' src='" + photoIcon + "'/>" + photoButtonHtml;
 
 						div.html(html);
-						feature_div.html(html);
 						div.attr('type', 'instagram');
 						div.fadeIn(200);
 
-						enableItemButtons(div, feature_div, popupData);
+						enableItemButton(div, popupData);
 
 						centerRollOverContent();
 
@@ -374,7 +532,6 @@ function Gallery()
 					case 'twitter':
 
 						div.css('background', '#2caae1');
-						feature_div.css('background', '#2caae1');
 
 						photoIcon = baseUrl + "img/icons/twitter-large.png";
 
@@ -404,11 +561,10 @@ function Gallery()
 							fcontent += "<img class='photo_icon' src='" + photoIcon + "'/>" + photoButtonHtml;
 
 						div.html(content);
-						feature_div.html(fcontent);
 						div.attr('type', 'twitter');
 						div.fadeIn(200);
 
-						enableItemButtons(div, popupData);
+						enableItemButton(div, popupData);
 
 						centerRollOverContent();
 
@@ -419,20 +575,6 @@ function Gallery()
 			});
 		}
 
-		function enableItemButtons(div, popupData){
-			$('.gallery_item_btn').unbind('mouseover').mouseover(function(e){
-				$(e.currentTarget).css('cursor','pointer');
-				$(e.currentTarget).prev('.item_rollover').fadeIn(200);
-			})
-
-			$('.gallery_item_btn').unbind('mouseout').mouseout(function(e){
-				$(e.currentTarget).prev('.item_rollover').fadeOut(200);
-			})
-
-
-        	$(div).find('.gallery_item_btn').click(function(e){openPopUp(popupData)})
-		}
-		
 
 		function loadMoreCircles() {					
 			feed_circles.more({
@@ -482,84 +624,66 @@ function Gallery()
 			})
 		}
 
-		function defineLayout(){
-			// switch(currentFilterType){
-			// 	case 'all':
-			// 		currentLayoutPath = baseUrl + 'layout/loadLayout' + current_add_layout;
-			// 	break;
-
-			// }
-		}
-
 		function loadLayout(){
 
 			$('.gallery_layout').remove();
 
 			switch(currentFilterType){
 				case 'all':
-
-					$.ajax({
-		        		type: 'get',
-		            	url: baseUrl + 'layout/loadLayout1',
-		            	dataType: 'html',
-		            	
-		            	success: function(data) {            
-							gallery_container.append(data);
-
-							$.ajax({
-				        		type: 'get',
-				            	url: baseUrl + 'layout/loadLayout2',
-				            	dataType: 'html',
-				            	
-				            	success: function(data) {            
-									gallery_container.append(data);
-									//parseDataToLayout();
-									centerRollOverContent();
-									
-				             	}
-				      		});
-							
-		             	}
-		      		});
+					createAllLayout();
+					getAllFeed();
+					
 				break;
 
 				case 'circle':
 				case 'friend':
 					createCircleLayout();
+					$.feed.get('bca-circle', parseCircleData, 4);
 				break;
 
 				case 'photo':
+					createPhotoLayout();
+					$.feed.get('bca-photo', parsePhotoData, 12);
+				break;
+
 				case 'instagram':
+					createPhotoLayout();
+					$.feed.get('bca-instagram', parseInstagramData, 12);
+				break;
+
 				case 'twitter':
 					createPhotoLayout();
+					$.feed.get('bca-twitter', parseTwitterData, 12);
 				break;
 			}
 
 		}
 
-		// function parseDataToLayout(){
+		function createAllLayout(){
+			$.ajax({
+	        		type: 'get',
+	            	url: baseUrl + 'layout/loadLayout1',
+	            	dataType: 'html',
+	            	
+	            	success: function(data) {            
+						gallery_container.append(data);
 
-		// 	switch(currentFilterType){
-		// 		case 'all':
-		// 			loadInitialCircles(false);
-		// 		break;
-
-		// 		case 'circle':
-		// 		case 'friend':
-		// 			loadInitialCircles(true);
-		// 		break;
-
-		// 		case 'photo':
-		// 		case 'instagram':
-		// 		case 'twitter':
-		// 			loadInitialPhotos();
-		// 		break;
-		// 	}
-
-		// 	getGalleryHeight();
-		// }
-
-
+						$.ajax({
+			        		type: 'get',
+			            	url: baseUrl + 'layout/loadLayout2',
+			            	dataType: 'html',
+			            	
+			            	success: function(data) {            
+								gallery_container.append(data);
+								//parseDataToLayout();
+								centerRollOverContent();
+								
+			             	}
+			      		});
+						
+	             	}
+	      		});
+		}
 		
 
 		function createCircleLayout(){
@@ -567,11 +691,8 @@ function Gallery()
 			circleLayout.addClass('layout_circle gallery_layout')
 						.appendTo(gallery_container);
 
-			if(currentFilterType!='all') $.feed.get('bca-circle', parseCircleData, 4);
+			//if(currentFilterType!='all') $.feed.get('bca-circle', parseCircleData, 4);
 		}
-
-
-
 
 		function createPhotoLayout(){
 			var photoLayout = $('<div>');
@@ -579,29 +700,7 @@ function Gallery()
 						.appendTo(gallery_container);
 		}
 
-		function createPhotoDivs(content, type, photoIcon, popupData){
-
-			// console.log(count%PHOTO_LAYOUT_COLUMN_NUM)
-
-			// if(count%PHOTO_LAYOUT_COLUMN_NUM == 1){
-			// 	var row = $('<div>')
-			// 	row.addClass('row')
-			// 		.appendTo('.layout_photo');
-			// }
-
-			// var col = $('<div>');
-			//  col.addClass('span3 photo_container gallery_item')
-			//  	.attr('type', type)
-			//  	.html(content)
-			//  	.appendTo(row);
-
-			//  if(type == "twitter") col.css('background', '#2caae1');
-			//  $(col.find('.photo_icon')).attr('src', photoIcon);
-
-			//  enableItemButtons(col, popupData);
-		}
-
-		function handlePhotoData(data){
+		function handleAllPhotoData(data){
 
 			$(data).each(function (i, v){
 				allPhotoData.push(v);
@@ -613,22 +712,7 @@ function Gallery()
 			  return ((aNum > bNum) ? -1 : ((aNum > bNum) ? 0 : 1));
 			});
 
-			parsePhotoData(allPhotoData);
-
-		}
-
-		function getGalleryHeight(){
-			var galleryHeight = 0;
-
-			$('.gallery_layout').each(function(i, v){
-				galleryHeight += ($(v).height() + (22*i));
-				
-			})
-
-			$('#gallery').height(galleryHeight);
-		}
-
-		function sortData(data){
+			parseAllPhotoData(allPhotoData);
 
 		}
 
@@ -670,18 +754,11 @@ function Gallery()
 			
 
 			$('.gallery_item').hide();
-			gallery_container.hide();
+			// gallery_container.hide();
 
 
 			$.feed();
 			fm_ready(function($, _) {
-
-				allPhotoData = new Array();
-
-				$.feed.get('bca-circle', parseCircleData, 3);
-				$.feed.get('bca-photo', handlePhotoData, 3);
-				$.feed.get('bca-twitter', handlePhotoData, 3);
-				$.feed.get('bca-instagram', handlePhotoData, 3);
 
 				initFilterButtons();
 				loadLayout();
