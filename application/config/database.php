237c02096996bab5896d27d08a0c4fb3473356a5
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2013-08-19 16:37:02 -0400

    changed prod db to click3x

--- application/config/database.php
+++ application/config/database.php
@@ -48,10 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['hostname'] = 'click3x.com';
+$db['default']['username'] = 'jtordsen';
+$db['default']['password'] = 'jasont1234';
+$db['default']['database'] = 'BCA';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
@@ -63,7 +63,6 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
-
 
 /* End of file database.php */
 /* Location: ./application/config/database.php */
