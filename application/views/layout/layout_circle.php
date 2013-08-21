2d71bd97403f62eda80e1ef46fcc83e47381ae66
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2013-08-21 12:46:01 -0400

    fb sharing stuff again

--- application/views/layout/layout_circle.php
+++ application/views/layout/layout_circle.php
@@ -18,9 +18,6 @@
 <div class="gallery_item_btn"></div>
 <div class='share_circle'>
 	<div class='share_text all_cap'>share</div>
-	<div onclick="$.popup_share({type:'facebook', url:''})" class='circle_fb_share_btn facebook_share_btn'><img src='<?php echo base_url(); ?>img/icons/facebook.png'/></div>
-	<div onclick="$.popup_share({type:'twitter', url:''})" class='circle_tw_share_btn twitter_share_btn'><img src='<?php echo base_url(); ?>img/icons/twitter.png'/></div>
+	<div onclick="$.popup_share({type:'facebook'})" class='circle_fb_share_btn facebook_share_btn'><img src='<?php echo base_url(); ?>img/icons/facebook.png'/></div>
+	<div onclick="$.popup_share({type:'twitter'})" class='circle_tw_share_btn twitter_share_btn'><img src='<?php echo base_url(); ?>img/icons/twitter.png'/></div>
 </div>
-
-
-			
