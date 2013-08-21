451629e2c874fe5a8a6bccba14aa49853bd627ff
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2013-08-21 12:10:24 -0400

    more sharing stuff

--- js/plugins.js
+++ js/plugins.js
@@ -195,29 +195,29 @@ $.extend(
     },
     popup_share: function(v)
     {
-        console.log("-- Posting to Facebook --");
+        if(v.type == "facebook"){
+            console.log("-- Posting to Facebook --");
 
-        var name = "test name";
-        var title = "test title";
-        var description = "test description";
+            FB.ui(
+              {
+                method: 'feed',
+                name: "We're Stronger Together.",
+                link: baseUrl+"#"+$.address.path(),
+                picture: baseUrl + 'img/assets/fb_share.jpg' ,
+                caption: 'Take action against breast cancer.' + (v.action != undefined ? " " + v.action : ""),
+                description: 'Create a Circle of Strength with those who support you most now.'
+              },
+              function(response) {
+                if (response && response.post_id) {
+                  //alert('Post was published.');
+                } else {
+                 // alert('Post was not published.');
+                }
+              }
+            );
+        } else if(v.type == "twitter"){
 
-        FB.ui(
-          {
-            method: 'feed',
-            name: '',
-            link: baseUrl+"#"+$.address.path(),
-            picture: baseUrl + 'img/assets/fb_share.jpg' ,
-            caption: 'Take action against breast cancer by [insert action]',
-            description: 'test'
-          },
-          function(response) {
-            if (response && response.post_id) {
-              //alert('Post was published.');
-            } else {
-             // alert('Post was not published.');
-            }
-          }
-        );
+        }
     }
 });
 
