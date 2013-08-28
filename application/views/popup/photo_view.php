4a126b206c30c7b7e0c7e6b70a652cc1d1b9d830
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2013-08-28 15:27:20 -0400

    weird code removed

--- application/views/popup/photo_view.php
+++ application/views/popup/photo_view.php
@@ -11,20 +11,10 @@
 			?>
 		</div>
 	</div>
-	<?
-		if($source =="instagram")
-			$url = "/photo/instagram/" . $id;
-		else{
-			if ($circle_id != null)
-				$url = "/circle/" . $circle_id . "/photo/bca/" . $id;
-			else
-				$url = "/photo/bca/" . $id;
-		}
-	?>
 	<div id="photo_footer">
 		<ul id="popup_footer_share">
 			<li>SHARE</li>
-			<li class="share-ico" id="popup_footer_share_facebook" onclick="$.popup_share({ photo_url:'<?php echo $url; ?>', post_type:'photo', type:'facebook'})"></li>
+			<li class="share-ico" id="popup_footer_share_facebook" onclick="$.popup_share({ photo_url:'<?php echo $photo_url; ?>', post_type:'photo', type:'facebook'})"></li>
 			<li class="share-ico" id="popup_footer_share_twitter" onclick="$.popup_share({post_type:'photo',type:'twitter'})"></li>
 		</ul>
 	</div>
