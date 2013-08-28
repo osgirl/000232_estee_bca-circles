ff994060ca21ff145fcb1ec4c5b1501bad3b0f6a
Jason Tordsen <jason@Jasons-MacBook-Pro.local>
2013-08-28 16:52:35 -0400

    forgot a space

--- js/plugins.js
+++ js/plugins.js
@@ -245,7 +245,7 @@ $.extend(
 
             var _title = "Circle of Strength";
             var _caption = "We're Stronger Together.";
-            var _description = 'Create a Circle of Strength with those who support you most now.' + goal;
+            var _description = 'Create a Circle of Strength with those who support you most now. ' + goal;
             var _link = baseUrl + indexPage + '#' + u;
 
             if(ismobile){
