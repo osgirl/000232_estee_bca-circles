072e7fac74459a5fbaedd82764b2089392b9700e
Sean Oh <ohsiwon@gmail.om>
2013-08-30 10:17:13 -0400

    revert gallery

--- js/util/gallery.js
+++ js/util/gallery.js
@@ -364,7 +364,7 @@ parse the circle data from feedmagnet and calls a route on our server to ccreate
 			console.log($data);
 
 
-			ored.photoFeed 	= $data;
+			ored.photoFeed 	= ored.photoFeed.concat($data);
 			var data 		= ored.getIdsFromFeed($data, "photo");
 			ored.photoIds 	= ored.photoIds.concat(data);
 			ored.addCookiePhotosToFeed();
