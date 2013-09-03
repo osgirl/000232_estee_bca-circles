947d116cf1a302dabe0302537a62c7ab67b11413
Sean Oh <ohsiwon@gmail.om>
2013-09-03 12:50:13 -0400

    IE9 Photo upload fixed

--- application/views/popup/photo_upload_view.php
+++ application/views/popup/photo_upload_view.php
@@ -34,8 +34,8 @@
 		<form id="file_form" name='form' method='POST' enctype='multipart/form-data'>
 			<input type="hidden" id="circle_id" name="circle_id" value="<? echo $circle_id?>">
 			<input type="hidden" id="users_fb_id" name="users_fb_id" value="<? echo $users_fb_id?>">
-			<input name="uploadFile" type="file" class="hidden" id="uploadFile"/>
 			<div id="button_wrapper">
+				<input name="uploadFile" type="file" class="hidden" id="uploadFile"/>
 				<div class="popup_round_button btn_browse" id="popup_btn_pink">BROWSE</div>
 				<div class="popup_round_button btn_cancel" id="popup_btn_pink">CANCEL</div>
 				<div class="popup_round_button btn_submit dim" id="popup_btn_pink">SUBMIT</div>
