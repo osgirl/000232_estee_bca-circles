a225abbd00fae88f9d4264634105ab6a431ccfb3
Sean Oh <sean@click3x.com>
2013-09-04 14:06:01 -0400

    About page css fix

--- application/views/popup/about_view.php
+++ application/views/popup/about_view.php
@@ -3,8 +3,7 @@
 	<div id="about_content">
 		<img id="about_header" src="<?= base_url(); ?>img/popups/about/about-header.jpg"/>
 		<img id="about_logo" src="<?= base_url(); ?>img/popups/about/about-logo.jpg"/>
-		<p id="about_indent_p">In 1992, during a time when thousands were dying from breast cancer yet it was only spoken about in whispers,</p>
-		<p>Evelyn H. Lauder launched The Estée Lauder Companies’ Breast Cancer Awareness (BCA) Campaign to raise awareness for the disease. She encouraged women everywhere to
+		<p>In 1992, during a time when thousands were dying from breast cancer yet it was only spoken about in whispers, Evelyn H. Lauder launched The Estée Lauder Companies’ Breast Cancer Awareness (BCA) Campaign to raise awareness for the disease. She encouraged women everywhere to
 practice breast health, while working tirelessly to raise funds for the critical research needed to find a cure. Twenty years later, in over 70 countries, The BCA Campaign is devoted to defeating breast cancer through education and
 medical research. The BCA Campaign will continue spreading its lifesaving awareness message globally in the hope of a world in which breast cancer no longer exists.</p>
 <span id="about_footnote">Mrs. Evelyn H. Lauder 1936 – 2011.</span>
