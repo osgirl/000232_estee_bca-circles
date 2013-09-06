1d51ed6db91a1a2ed66900c86d476ec9e2f85c70
Mili Kuo <mili@click3x.com>
2013-09-06 11:09:47 -0400

    language new sql file

--- application/controllers/language.php
+++ application/controllers/language.php
@@ -15,22 +15,6 @@ class Language extends CI_Controller {
 
 			$language = $this->post['language'];
 
-			$query = $this->db->select("languages.*")	
-				->from("languages")
-				->where_in("languages.id",$feedArr)
-				->get();
-
-			$query = $this->db->query("UPDATE goals SET taken_number=taken_number + 1 WHERE id = '$ref_goal_id'"); 
-
-			if ($result){
-				$data['id'] = $result;
-				$data['fb_id'] = $this->post['users_fb_id'];
-				$data['goal'] = $this->post['goal'];
-				$data['goal_id'] = $ref_goal_id;
-				$data['language'] = $this->post['language'];
-				$data['country'] = $this->post['country'];
-				echo json_encode($data);
-			}
 			else
 				echo 'Write failed';
 		 }
