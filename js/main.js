a9b4e42a1918d8a7dd7bf1d7eddca766bac4b220
Mili Kuo <mili@click3x.com>
2013-09-12 13:40:09 -0400

    portuguese

--- js/main.js
+++ js/main.js
@@ -217,6 +217,7 @@ function translatePage(){
 	$.language.load(function(e){
 
 		if(selectedLanguage == "it") NAME_TEXTFIELD_WIDTH = 180;
+		if(selectedLanguage == "pt") NAME_TEXTFIELD_WIDTH = 155;
 
 		languageData = e;
    		loadLanguageToElements(e);
