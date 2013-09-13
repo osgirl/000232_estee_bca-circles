af013a9f5cb03eb989eb5d931b6306a76f11baf1
Sean Oh <sean@click3x.com>
2013-09-13 12:11:27 -0400

    Twitter share translation

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -7,12 +7,13 @@ class Home extends CI_Controller {
 		$this->load->view('home_view');
 	}
 
-	public function twitter_share($url ="", $goal = "", $hashtag_before_url = false)
+	/*
+	public function twitter_share($url ="", $goal, $hashtag_before_url = false)
 	{
 		$pt = preg_replace('/_/i','/', $url);
 		$pt = preg_replace('/%3F/i','?', $pt);
 		$pt = preg_replace('/%5E/i','=', $pt);
-		$long_url 	= base_url() . index_page() . '/#' . $pt;
+		$long_url 	= base_url() . index_page() . '/#' . urldecode($pt);
 
 		$short_url 	= file_get_contents( "https://api-ssl.bitly.com/v3/shorten?access_token=".BITLY_ACCESS_TOKEN."&longUrl=".urlencode($long_url)  );
 		$short_url 	= json_decode($short_url);
@@ -23,9 +24,24 @@ class Home extends CI_Controller {
 			$title = $title." ".$short_url . " " .SHARE_HASHTAG;
 		}
 		else{
-			$title = "We Will: ".$title." ".SHARE_HASHTAG." ".$short_url;
+			$title = $title." ".SHARE_HASHTAG." ".$short_url;
 		}
+		echo 'test';
+		print_r($goal);
+		echo $title;
+		header( 'Location: https://twitter.com/intent/tweet?text='.$title  );
+	}
+	*/
 
-		header( 'Location: https://twitter.com/intent/tweet?text='.$title );
+	public function bitly_url($url)
+	{
+		$pt = preg_replace('/_/i','/', $url);
+		$pt = preg_replace('/%3F/i','?', $pt);
+		$pt = preg_replace('/%5E/i','=', $pt);
+		$long_url 	= base_url() . index_page() . '/#' . urldecode($pt);		
+		$short_url 	= file_get_contents( "https://api-ssl.bitly.com/v3/shorten?access_token=".BITLY_ACCESS_TOKEN."&longUrl=".urlencode($long_url)  );
+		$short_url 	= json_decode($short_url);
+		$short_url 	= $short_url->data->url;
+		echo $short_url;
 	}
 }
