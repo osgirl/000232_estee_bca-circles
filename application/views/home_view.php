f9b9748f7a08fd0863006cd532552780936fab59
Mili Kuo <mili@click3x.com>
2013-10-08 16:31:08 -0400

    new image

--- application/views/home_view.php
+++ application/views/home_view.php
@@ -76,7 +76,7 @@
 	                <div id='top_content'>
 		             	<div class="row">
 			                <div class="top_content_span span6">
-			                    <div id="big_image"><img src='<?= base_url(); ?>img/pics/stronger-together-new.jpg'/>
+			                    <div id="big_image"><img src='<?= base_url(); ?>img/pics/stronger-together-new2.jpg'/>
 				                    <div id="tagline">
 				                    	<div language_id="we_are_stronger_together" language_location="home" class="stronger_together all_cap">we’re stronger<br /><span style="font-size:165%">together</span></div>
 				                    	<div language_id="main_image_copy" language_location="home" class="copy_text">Together, take action in defeating breast cancer by inviting your friends to join your circle of strength. Show how you're fighting breast cancer by adding a photo, creating a circle, or tagging #BCAstrength.</div>
